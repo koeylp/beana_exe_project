1e6fbc0750453f16bfb6236e06d11717a3b4d885
K <thekhoi.k@gmail.com>
2023-10-17 21:23:56 +0700

    config port cors

--- src/main/java/com/exe201/beana/config/SecurityConfig.java
+++ src/main/java/com/exe201/beana/config/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
-                        config.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:5173"));
                         config.setAllowedMethods(Collections.singletonList("*"));
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
