7d8bfca16d2f3c35e55c84ce38450d3eec848135
K <thekhoi.k@gmail.com>
2023-10-20 23:01:20 +0700

    set secure flag

--- src/main/java/com/exe201/beana/BeanaApplication.java
+++ src/main/java/com/exe201/beana/BeanaApplication.java
@@ -2,13 +2,22 @@ package com.exe201.beana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+
 
 @SpringBootApplication
 public class BeanaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BeanaApplication.class, args);
+    }
+
+    @Bean
+    public DefaultCookieSerializer defaultCookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setUseSecureCookie(true); // This will ensure the secure flag is set for cookies
+        return serializer;
     }
 
 }
