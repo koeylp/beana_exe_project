bee8e077e935e2a88891118315468c297f526061
K <thekhoi.k@gmail.com>
2023-10-20 23:27:24 +0700

    config cors

--- src/main/java/com/exe201/beana/BeanaApplication.java
+++ src/main/java/com/exe201/beana/BeanaApplication.java
@@ -2,8 +2,6 @@ package com.exe201.beana;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.session.web.http.DefaultCookieSerializer;
 
 
 @SpringBootApplication
@@ -13,11 +11,6 @@ public class BeanaApplication {
         SpringApplication.run(BeanaApplication.class, args);
     }
 
-    @Bean
-    public DefaultCookieSerializer defaultCookieSerializer() {
-        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
-        serializer.setUseSecureCookie(true); // This will ensure the secure flag is set for cookies
-        return serializer;
-    }
+
 
 }
--- src/main/java/com/exe201/beana/config/MySameSiteConfiguration.java
+++ src/main/java/com/exe201/beana/config/MySameSiteConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.boot.web.servlet.server.CookieSameSiteSupplier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration(proxyBeanMethods = false)
+//@Configuration(proxyBeanMethods = false)
 public class MySameSiteConfiguration {
     @Bean
     public CookieSameSiteSupplier applicationCookieSameSiteSupplier() {
