05eef1734a65de9c1f8372a692824a3546f64d09
K <thekhoi.k@gmail.com>
2023-11-10 09:30:43 +0700

    validate phone number Vietnam format

--- src/main/java/com/exe201/beana/controller/FileUploadController.java
+++ src/main/java/com/exe201/beana/controller/FileUploadController.java
@@ -5,7 +5,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/com/exe201/beana/dto/RegisterRequestDto.java
+++ src/main/java/com/exe201/beana/dto/RegisterRequestDto.java
@@ -31,7 +31,7 @@ public class RegisterRequestDto {
     @NotBlank(message = "The property is not null or whitespace")
     private String email;
 
-    @Pattern(regexp = "(^$|[0-9]{10})", message = "Phone must be only number and including 10 numbers")
+    @Pattern(regexp = "((84|0[3|5|7|8|9])+([0-9]{8})\\b)", message = "Phone must be start with 0 or 84 (Ex: 03, 05, 07, 08, 09 or replace 0 with 84) only number and including 10 numbers with 0 and 11 with 84")
     @NotEmpty(message = "Phone is required")
     @NotBlank(message = "The property is not null or whitespace")
     private String phone;
